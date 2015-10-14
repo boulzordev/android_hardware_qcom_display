10be56ccfdc2621bca9fde0a66da82784991e1b3
Benjamin Legrand <android@legrand.ws>
2015-10-14 08:52:27 +0200

    [liblight] Do not add if device defines TARGET_PROVIDES_LIBLIGHT
    Change-Id: I144f5b70bffb060cb7971d16722f10b0dc3f7b35

--- liblight/Android.mk
+++ liblight/Android.mk
@@ -11,6 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+#
+
+ifeq ($(TARGET_PROVIDES_LIBLIGHT),)
 
 LOCAL_PATH:= $(call my-dir)
 # HAL module implemenation stored in
@@ -25,3 +29,4 @@ LOCAL_MODULE := lights.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+endif
