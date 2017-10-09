2cc9790e2f1044e723033b71ff8ba06c0e8e2a9b
Vachounet <vachounet@live.fr>
2017-10-09 10:34:10 +0200

    Android.mk: update for omni

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-display))
+ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),caf-msm8937)
 
 sdm-libs := sdm/libs
 display-hals := include libqservice libqdutils $(sdm-libs)/utils $(sdm-libs)/core
